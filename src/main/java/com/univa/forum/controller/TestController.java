f2182c2140360cc5b16ae5707aa9c282261f6098
qwanchoi <chlrbdhks92@gmail.com>
2020-11-27 15:33:04 +0900

    페이지 렌더링 확인

---
+++ src/main/java/com/univa/forum/controller/TestController.java
@@ -0,0 +1,13 @@
+package com.univa.forum.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class TestController {
+	
+	@GetMapping("/hello")
+	public String TestPage() {
+		return "test";
+	}
+}
