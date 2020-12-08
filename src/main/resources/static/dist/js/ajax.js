cdcf6351772082ed230beaf19ea5ef858cdc4389
tjgmlfbs124 <tjgmlfbs124@gmail.com>
2020-12-08 13:35:48 +0900

    메인페이지 개발중

--- src/main/resources/static/dist/js/ajax.js
+++ src/main/resources/static/dist/js/ajax.js
@@ -15,3 +15,21 @@ function postAPI(url, data, successCallback){
 	      }
 	  }) // End Ajax Request
 }
+
+function getAPI(url, data, successCallback){
+	  $.ajax({
+	      type : 'GET',
+	      url : url,
+	      dataType : 'text',
+	      data : data,
+	      contentType : false,
+	      processData: false,
+	      success : function(result) {
+	        successCallback(result);
+	      },
+	      error: function(request, status, error) {
+	        console.log("Error post Api state : " + status);
+	        console.log("Error post Api error : " + error);
+	      }
+	  }) // End Ajax Request
+}
